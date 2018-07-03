6a98d520c0982360bc9c9c487821307a5af4b17c
emilyg406 <40281360+emilyg406@users.noreply.github.com>
2018-07-03 15:40:51 -0700

    fixed a thing

--- Integration - Trapezoid.py
+++ Integration - Trapezoid.py
@@ -8,15 +8,15 @@ n=1000
 
 # Find the integral of a function
 # Define function
-def function(x);
+def function(x):
     y=x**2
     return y
 
 #Trapezoid Rule on function
-def trapezoid(y,xl,xu,n);
+def trapezoid(y,xl,xu,n):
     dx=(xu-xl)/n
     sum=0
-    for i in range (0,n+1);
+    for i in range (0,n+1):
         if i==0 or i==n+1:
             trap=y(xl+i*dx)
             sum=sum+simp
@@ -24,10 +24,10 @@ def trapezoid(y,xl,xu,n);
             trap=2*y(xl+i*dx)
     return sum*.5*dx
 
-def graph(y,xl,xu);
+def graph(y,xl,xu):
     x=np.arange(xl,xu,(xu-xl)/n)
     y=[]
-    for xvals in x;
+    for xvals in x:
         y.append(y(xvals))
     plot.plot(xy)
     plot.grid(True)
