5da0a97eba9dd50f1267edd0459f8a67a2d7f73f
emilyg406 <40281360+emilyg406@users.noreply.github.com>
2018-07-04 17:33:58 -0700

    Eulers

---
+++ Eulers.py
@@ -0,0 +1,18 @@
+import math as m
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Euler's Method
+
+def euler(t0,y0,n,tfinal):
+    dt=(tfinal-t0)/n
+    y=[]
+    t=[]
+    y.append(y0)
+    t.append(t0)
+    for i in range (t0,n)):
+        t.append((i+1)*dt)
+        y.append(y[i]+y[i]*t[i]*dt)
+    retun plt.plot(t,y)
+
+print(euler(0,2,1000,10))
