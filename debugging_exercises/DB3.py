c13cd0e7f5caf355e4a0140eb363840ff16d8dc5
AJ Stewart <allen.j.stwrt@gmail.com>
2018-07-09 18:02:51 -0700

    Debugging Files
    All of these files have errors in them. Find the errors and correct the code.

---
+++ debugging_exercises/DB3.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  5 12:44:56 2018
+
+@author: allen
+"""
+######################################
+#Below is code that is supposed to compute the Eulers method 
+#I have purposely introduced an error(s) into the code
+#The Purpose of this code is to practice debugging some common errors
+#I won't give any hints, but I would like you to find the error and correct it
+#You should identify all errors and the line in which they occur in your commit message
+
+import math as m
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Euler's Method
+
+def function(y,t):
+	dy=yt
+	return dy
+
+def euler(t0,y0n,tfinal):
+    dt=(tfinal-t0)/n
+    y=[]
+    t=[]]
+    y.append(y0)
+    t.append(t0)
+    for i in range (0,n):
+        t.append((i+1))*dt)
+        y.append(y[i]+function(yi,t[i])*dt)
+    return plt.plot(t,y)
---
+++ debugging_exercises/DB6.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  5 12:44:56 2018
+
+@author: allen
+"""
+######################################
+#Below is code that is supposed to compute the Eulers method 
+#I have purposely introduced an error(s) into the code
+#The Purpose of this code is to practice debugging some common errors
+#I won't give any hints, but I would like you to find the error and correct it
+#You should identify all errors and the line in which they occur in your commit message
+
+import math as m
+import numpy as np
+import matplotlib.pyplot as plt
+
+#Euler's Method
+
+def function(y,t):
+	dy=y*t
+	return dy
+
+def euler(t0,y0,n,tfinal):
+    dt=(tfinal-t0)/n
+    y=[]
+    t=[]
+    y.append(y0)
+    t.append(t0)
+    for i in range (1,n):
+        t.append(i*dt)
+        y.append(y[i]+function(y[i],t[i])*dt)
+    return plt.plot(t,y)
