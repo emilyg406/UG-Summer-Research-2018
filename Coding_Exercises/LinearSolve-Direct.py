e01ff1123534ef0eba04b258a1bc3ccb20e21310
emilyg406 <40281360+emilyg406@users.noreply.github.com>
2018-08-01 16:22:49 -0700

    DONE

--- Coding_Exercises/LinearSolve-Direct.py
+++ Coding_Exercises/LinearSolve-Direct.py
@@ -17,50 +17,45 @@ Created on Tue Jul 10 07:59:29 2018
 #2. Provided that A has an inverse, we find A^-1.
 #3. Multiply B by A^-1 on the left.
 
-
 #Import the appropriate libraries
 import math
 import matplotlib.pyplot as plt
 import numpy as np
 
-def LinearSolve(A,b): # A is a matrix, b is a vector
-	A=np.array([])
-	b=np.array([])
-	n=len(A)
-	x=np.array([])
+def LinearSolve(A,b): # A is a matrix, b is a vector)
+    n=len(A)
+    x=np.zeros(n)
+    #Create an If statement that will return an appropriate error if det(A)=0
+    if np.linalg.det(A)==0:
+        return "Error - Determinant = 0"
 
-	#Create an If statement that will return an appropriate error if det(A)=0
-	if np.linalg.det(A)==0:
-		return "Error - Determinant = 0"
+    #Create an If statement that will return an appropriate error if the dimensions of A and b don't align
+    elif len(A[0])!=len(b):
+        return "Error: Incompatible dimensions"
+    elif len(A)!=len(A[0]):
+        return "Error: Incompatible Dimensions"
 
-	#Create an If statement that will return an appropriate error if the dimensions of A and b don't align
-	elif len(A)!=len(b):
-		return "Error: Incompatible dimensions"
-	elif len(A)!=len(A[]):
-		return "Error: Incompatible Dimensions"
+    #Find the inverse matrix A^-1
+    else:
+        Ainv=np.linalg.inv(A)
 
-	#Find the inverse matrix A^-1
-	else:
-		Ainv=np.linalg.inv(A)
+    #Multiply A^-1*b. Make sure this multiplication is in fact matrix times vector multiplication
+    for i in range (0,n):
+        for j in range (0,n):
+            x[i]=x[i]+Ainv[i][j]*b[j]
+            sol=x
+    return sol #Return the solution vector 
 
-	#Multiply A^-1*b. Make sure this multiplication is in fact matrix times vector multiplication
-	for i in range (0,n):
-		for j in range (0,n):
-			x[i]=b[j]*A[i][j]
-			sol=x
-			return sol #Return the solution vector 
+print(LinearSolve([[4.523,7.23,7,8],[-3.1,4,5,2],[5,6,0.75,11],[-2,3,-1,4]],[1,2,3,4]))
 
-
-	
 ########################################################
 
 #Print out test of code for each equation
 
 #A=[[1,2],[3,4]], b=[5,6] Solution:[-4,4.5]
-	
+    
 #A=[[1,2,3],[4,5,6],[7,8,9]], b=[10,11,12] Solution: This will return an error, det(A)=0
-	
+    
 #A=[[4,5,7,8],[3,4,5,2],[5,6,7,11],[-2,3,-1,4]], b=[3,4,5,-1,3] Solution: This will return an error, A and b are different sizes
-	
+    
 #A=[[4.523,7.23,7,8],[-3.1,4,5,2],[5,6,0.75,11],[-2,3,-1,4]], b=[1,2,3,4] Solution: [-0.331268,1.06199,-0.609205,-0.114425]
-
