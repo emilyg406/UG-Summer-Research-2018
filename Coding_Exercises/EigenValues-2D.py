83fc736c03629061dcb303df77266158f214e892
emilyg406 <40281360+emilyg406@users.noreply.github.com>
2018-08-01 10:45:56 -0700

    Edits - to go over w/ AJ

--- Coding_Exercises/EigenValues-2D.py
+++ Coding_Exercises/EigenValues-2D.py
@@ -12,7 +12,7 @@ Created on Tue Jul 10 07:59:29 2018
     #But since we are dealing with a 2x2 system, being non-invertible
     #Is equivalent to the columns of the matrix being multiples of each other
     #Thus we can assume that x=[1,a] for some constant a
-#3. Normalize both of the eigenvectors, i.e. reduce them so they have norm equal to 1	
+#3. Normalize both of the eigenvectors, i.e. reduce them so they have norm equal to 1    
 #################################################################
 #One important thing to note here is that we might have complexe solutions
 #This is fine we want to include them in our answer
@@ -22,45 +22,63 @@ Created on Tue Jul 10 07:59:29 2018
 #Import the appropriate libraries
 import numpy as np
 import math as m
-import matplotlib.pyplot as plot
 
 def EVal(A): # A is a 2x2 matrix
-	A=np.array([])
-	sol=0
-	#Create an If statement that will return an appropriate error if A is not 2x2
-	if len(A)!=2):
-		sol="Matrix A is not 2x2 - cannot determine eigenvalues"
-	elif len(A[i])!=2:
-		sol="Matrix A is not 2x2 - cannot determine eigenvalues"
-	#Solve the characteristic equation
-	else:
-		a=1
-		b=A[1][1]+A[2][2]
-		c=(A[1][1]*A[2][2])-(A[1][2]*A[2][2])
-		det=b*2-a*c
-		if det>=0:
-			sol1=(-b+np.sqrt(det))/2
-			sol2=(-b-np.sqrt(det))/2
-		else:
-			re=-b/2
-			im=(np.sqrt(-det))/2
-			sol1=re+im"i"
-			sol2=re-im"i"
-		
-	#Find a solution to (A-lambda*I)x=0 for both solutions
-	
-	#Normalize your solutions	   	 
-	
-	return sol #Return the solution in the form [lambda_1,v_1],[lambda_2,v_2]
+    
+    #Create an If statement that will return an appropriate error if A is not 2x2
+    if len(A)!=2:
+        sol="Matrix A is not 2x2 - cannot determine eigenvalues"
+    elif len(A[i])!=2:  #is this correct? Should I change A[i]?
+        sol="Matrix A is not 2x2 - cannot determine eigenvalues"
+    
+    
+    #Solve the characteristic equation
+    else:
+        a=1
+        b=A[0][0]+A[1][1]
+        c=(A[0][0]*A[1][1])-(A[0][1]*A[1][0])
+        det=b*2-2*a*c
+        if det>=0:
+            #Find a solution to (A-lambda*I)x=0 for both solutions
+            #Normalize your solutions
+            lamda1=(-b+np.sqrt(det))/2
+            lamda2=(-b-np.sqrt(det))/2
+            B1=[[A[0][0]-lamda1,A[0][1]],[A[1][0],A[1][1]-lamda1]]
+            B2=[[A[0][0]-lamda2,A[0][1]],[A[1][0],A[1][1]-lamda2]]
+            v1=np.linalg.solve(B1,[0,0])
+            v2=np.linalg.solve(B2,[0,0])
+            v1_mag=m.sqrt(v1[0]**2+v1[1]**2)
+            v2_mag=m.sqrt(v2[0]**2+v2[1]**2)
+            v1_norm=v1/v1_mag
+            v2_norm=v2/v2_mag
+        
+        else:
+            re=-b/2
+            im=(np.sqrt(-det))/2
+            lamda1=re+im
+            lamda2=re-im
+            #This part - how do I deal with the imaginary portion?
+            B1=[[A[0][0]-re,A[0][1]],[A[1][0],A[1][1]-re]]
+            B2=[[A[0][0]-re,A[0][1]],[A[1][0],A[1][1]-re]]
+            v1=np.linalg.solve(B1,[0,0])
+            v2=np.linalg.solve(B2,[0,0])
+            v1_mag=m.sqrt(v1[0]**2+v1[1]**2)
+            v2_mag=m.sqrt(v2[0]**2+v2[1]**2)
+            v1_norm=v1/v1_mag
+            v2_norm=v2/v2_mag
+            
+    sol=[lamda1,v1_norm],[lamda2,v2_norm]
+    
+    return sol #Return the solution in the form [lambda_1,v_1],[lambda_2,v_2]
 
-
-	
+print(EVal([[1,2],[3,4]]))
+    
 ########################################################
 
 #Print out test of code for each equation
 
 #A=[[1,2],[3,4], Solution: [5.37228,[0.415974, 0.909377]], [-0.372281,[-0.824565, 0.565767]]
-	
+    
 #A=[[0,1],[-1,0],Solution: complex solutions [i,[- 0.707107i, 0.707107],[-i,[0.707107i, 0.707107]]
 
 #A=[[4,5,13],[3,4,5],[5,6,7]], Should return an error
